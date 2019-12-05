cbdd945f6686a154af6cb892197ed16486c0dc06
Arturo Alonso <alonsoarturo@ou.edu>
2019-12-04 21:31:40 -0600

    Removed unnecessary line of code' git status

--- Main.java
+++ Main.java
@@ -12,7 +12,6 @@ import javafx.stage.Popup;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
