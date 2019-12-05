886416f76ab8e4e74595b6ae7f66345d06a816da
Arturo Alonso <alonsoarturo@ou.edu>
2019-12-04 22:17:37 -0600

    Slight modification to verb textfield

--- Main.java
+++ Main.java
@@ -325,7 +325,7 @@ public class Main extends Application {
 			grid.add(ninthQLabel, 2, 10);
 			grid.add(verb1Field, 3, 10);
 			grid.add(verb2Field, 3, 11);
-			grid.add(verb3Field, 3, 13);
+			grid.add(verb3Field, 3, 12);
 			grid.add(verb4Field, 3, 14);
 			
 			grid.setHgap(10);
